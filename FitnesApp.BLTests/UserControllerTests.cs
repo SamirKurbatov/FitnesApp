15b7c171dddc3e98d9254425c464efa917f93b59
SamirKurbatov <samir.kurbatov.03@inbox.ru>
2023-05-07 17:24:33 +0300

    написали тесты

---
+++ FitnesApp.BLTests/UserControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FitnesApp.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnesApp.BL.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+    
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveUsersTest()
+        {
+            // Arrange
+            var userName = Guid.NewGuid().ToString();
+            // Act
+            var controller = new UserController(userName);
+
+            // Assert
+            Assert.AreEqual(userName, controller.CurrentUser.FirstName);
+        }
+    }
+}
